221e7d9cad61955780a31eb6ef8028979b37629f
Hans Ren <171357351@qq.com>
2018-09-29 10:13:25 +0800

    Update and rename findRPeak.py to findQRS.py

---
+++ findQRS.py
@@ -0,0 +1,95 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu Sep  6 14:35:57 2018
+
+@author: hans
+"""
+import matplotlib.pyplot as plt
+import numpy as np
+def findR(TimeList,ECGlist):
+    RMax=max(ECGlist)
+    RMaxIndex=[]
+    RPeakIndexList=[]
+    xMax=[]
+    yMax=[]
+    for i in range(len(ECGlist)):
+        if ECGlist[i]>RMax*0.5: #0.5 is depended on situation.
+            RMaxIndex.append(i)
+    tempIndex=[RMaxIndex[0]]
+    for i in range(1,len(RMaxIndex)+1):
+        if i<len(RMaxIndex):
+            if RMaxIndex[i]-RMaxIndex[i-1]<50:
+                tempIndex.append(RMaxIndex[i])
+                continue
+            elif len(tempIndex)==0:
+                tempIndex.append(RMaxIndex[i])
+        tempECG=[]
+        for ind in tempIndex:
+            tempECG.append(y[ind])
+        tempRPeakIndex=tempECG.index(max(tempECG))
+        RPeakIndexList.append(tempIndex[tempRPeakIndex])
+        tempIndex=[]
+    for j in RPeakIndexList:
+        xMax.append(np.array(TimeList)[j])
+        yMax.append(np.array(ECGlist)[j])
+    return xMax,yMax,RPeakIndexList
+def findQS(TimeList,ECGlist,RPeakIndexList):
+    fs=250
+    w1=fs/10
+    w2=fs/5
+    l=len(ECGlist)
+    QxMin=[]
+    QyMin=[]
+    SxMin=[]
+    SyMin=[]
+    for ind in RPeakIndexList:
+        if ind<w1:
+            w1=ind
+        elif ind < w2:
+            w2=ind
+        if ind+w1>l:
+            w1=l-ind-1
+        if ind+w2>l:
+            w2=l-ind-1
+        w1TempQ=[]
+        w1TempS=[]
+        w2TempQ=[]
+        w2TempS=[]
+        for i in range(int(w1)):
+            w1TempQ.append([ECGlist[ind-i],ind-i])
+            w1TempS.append([ECGlist[ind+i],ind+i])
+        for j in range(int(w2)):
+            w2TempQ.append([ECGlist[ind-i],ind-i])
+            w2TempS.append([ECGlist[ind+i],ind+i])
+        w1Qmin=min(w1TempQ)
+        w1Smin=min(w1TempS)
+        w2Qmin=min(w2TempQ)
+        w2Smin=min(w2TempS)
+        if w1Qmin==w2Qmin:
+            QxMin.append(TimeList[w1Qmin[1]])
+            QyMin.append(ECGlist[w1Qmin[1]])
+        elif w1Qmin>w2Qmin:
+            QxMin.append(TimeList[w2Qmin[1]])
+            QyMin.append(ECGlist[w2Qmin[1]])
+        elif w1Qmin<w2Qmin:
+            QxMin.append(TimeList[w1Qmin[1]])
+            QyMin.append(ECGlist[w1Qmin[1]])
+        if w1Smin==w2Smin:
+            SxMin.append(TimeList[w1Smin[1]])
+            SyMin.append(ECGlist[w1Smin[1]])
+        elif w1Smin>w2Smin:
+            SxMin.append(TimeList[w2Smin[1]])
+            SyMin.append(ECGlist[w2Smin[1]])
+        elif w1Smin<w2Smin:
+            SxMin.append(TimeList[w1Smin[1]])
+            SyMin.append(ECGlist[w1Smin[1]])
+    return QxMin,QyMin,SxMin,SyMin
+            
+def findQRS(TimeList，ECGlist):
+    num=1000
+    x=np.array(TimeList[0:num])
+    y=np.array(ECGlist[0:num])
+    RxMax,RyMax,RPeakIndexList=findR(TimeList[0:num],ECGlist[0:num])
+    QxMin,QyMin,SxMin,SyMin=findQS(TimeList[0:num],ECGlist[0:num],RPeakIndexList)
+    plt.plot(x,y,'b',RxMax, RyMax, 'or',QxMin,QyMin,'xg',SxMin,SyMin,'.k')
+    plt.show()
